08fac7cd472049247d00080ca5f94cf649f5035b
Chris Davis <github@vu6.me>
2017-08-21 18:54:09 -0500

    Initial setup

---
+++ js/game.js
@@ -0,0 +1,19 @@
+// js/game.js
+
+// https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_breakout_game_Phaser
+
+var oGame = new Phaser.Game(480,320,Phaser.AUTO,null,{
+	preload:	preload,
+	create:		create,
+	update:		update
+});
+
+function preload(){
+
+}
+function create(){
+
+}
+function update(){
+
+}
