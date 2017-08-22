d83b528f7931077b13bf167519cbdfc3ed88d62b
Chris Davis <github@vu6.me>
2017-08-22 07:45:29 -0500

    added paddle

--- js/game.js
+++ js/game.js
@@ -2,29 +2,43 @@
 
 // https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_breakout_game_Phaser
 
-var oGame = new Phaser.Game(480,320,Phaser.AUTO,null,{
-	preload:	preload,
-	create:		create,
-	update:		update
+var oGame = new Phaser.Game(480, 320, Phaser.AUTO, null, {
+	preload: preload,
+	create: create,
+	update: update
 });
 
-function preload(){
+function preload() {
 	oGame.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 	oGame.scale.pageAlignHorizontally = true;
 	oGame.scale.pageAlignVertically = true;
 	oGame.stage.setBackgroundColor('#eee');
 
-	bmBall = oGame.add.bitmapData(20,20);
-	bmBall.circle(10,10,10,'#00a5e6');
+	bmdBall = oGame.add.bitmapData(20, 20);
+	bmdBall.circle(10, 10, 10, '#00a5e6');
+
+	bmdPaddle = oGame.add.bitmapData(75, 10);
+	bmdPaddle.ctx.fillStyle = '#00a5e6';
+	bmdPaddle.ctx.fillRect(0, 0, 75, 10);
 }
-function create(){
-	sprBall = oGame.add.sprite(50,50,bmBall);
+function create() {
+	sprBall = oGame.add.sprite(oGame.world.width * 0.5, oGame.world.height - 25, bmdBall);
+	sprBall.anchor.set(0.5);
+
+	sprPaddle = oGame.add.sprite(oGame.world.width * 0.5, oGame.world.height - 5, bmdPaddle);
+	sprPaddle.anchor.set(0.5, 1);
+
 	oGame.physics.startSystem(Phaser.Physics.ARCADE);
-	oGame.physics.enable(sprBall,Phaser.Physics.ARCADE);
-	sprBall.body.velocity.set(150,150);
+
+	oGame.physics.enable(sprBall, Phaser.Physics.ARCADE);
+	sprBall.body.velocity.set(150, -150);
 	sprBall.body.collideWorldBounds = true;
 	sprBall.body.bounce.set(1);
+
+	oGame.physics.enable(sprPaddle, Phaser.Physics.ARCADE);
+	sprPaddle.body.immovable = true;
 }
-function update(){
-	
+function update() {
+	oGame.physics.arcade.collide(sprBall, sprPaddle);
+	sprPaddle.x = oGame.input.x || oGame.world.width * 0.5;
 }
